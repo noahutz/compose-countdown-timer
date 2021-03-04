7ed71825882c3f7a43028a2f2e00f764f7f508a5
Noah Utzurrum <noah.utzurrum@gmail.com>
2021-03-04 20:52:17 +1100

    Fixed issue on ExampleInstrumentedTest with missing Experimental annotations

--- app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
@@ -15,6 +15,8 @@
  */
 package com.example.androiddevchallenge
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -26,6 +28,8 @@ import org.junit.runner.RunWith
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExperimentalFoundationApi
+@ExperimentalAnimationApi
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @get:Rule
